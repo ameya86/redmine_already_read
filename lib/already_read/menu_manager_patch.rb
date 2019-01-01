7e41bb4d6eec43754e57f34fad2ef8405238efbd
tkusukawa <t@kusukawa.jp>
2019-01-01 11:58:50 +0900

    Display unread counter

--- lib/already_read/menu_manager_patch.rb
+++ lib/already_read/menu_manager_patch.rb
@@ -12,7 +12,10 @@ module AlreadyReadMenuManagerPatch
   module InstanceMethods
     def render_menu_with_already_read(menu, project=nil)
       html = render_menu_without_already_read(menu, project)
-      if menu == :top_menu
+      if menu == :top_menu && html =~ /^(.*<a.*?class="my-page".*?)(<\/a.*)$/
+        html1 = $1
+        html2 = $2
+
         query = IssueQuery.new(:name => l(:label_assigned_to_me_issues), :user => User.current)
         query.add_filter 'assigned_to_id', '=', ['me']
         assigned_to_me_count = query.issue_count
@@ -22,7 +25,7 @@ module AlreadyReadMenuManagerPatch
 
         counter_str = %Q{ <span class="ar_counter unread" title="　　#{l(:label_already_read_unread)}">#{unread_count}</span>}
         counter_str << %Q{/<span class="ar_counter assigned_to_me" title="　　#{l(:label_assigned_to_me_issues)}">#{assigned_to_me_count}</span>}
-        html.gsub!(/(<a.*?class="my-page".*?)(<\/a)/, '\1' + counter_str + '\2')
+        html = html1 + counter_str + html2
       end
       return html.html_safe
     end
